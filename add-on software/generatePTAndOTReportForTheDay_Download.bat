8cbc483b60e68ace63a4ad1af928534ade0f0f3e
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-05 14:19:08 +0800

    Add files via upload

--- add-on software/generatePTAndOTReportForTheDay_Download.bat
+++ add-on software/generatePTAndOTReportForTheDay_Download.bat
@@ -1,4 +1,5 @@
 set mainDirectory=%~dp0
 cd /d %mainDirectory%
 java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect "download" http://localhost/ "C:/Usbong/unit/add-on software/output/SVGH/" 
+#pause
 exit
--- add-on software/generatePTAndOTReportForTheDay_Upload.bat
+++ add-on software/generatePTAndOTReportForTheDay_Upload.bat
@@ -1,6 +1,6 @@
 set mainDirectory=%~dp0
 cd /d %mainDirectory%
-java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect "upload" http://localhost/ "C:/Usbong/unit/add-on software/output/SVGH/"*.txt
+java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect "upload" http://localhost/ "C:/Usbong/unit/add-on software/output/SVGH/client/"*.txt
 cd ..
 %2
 PAUSE
